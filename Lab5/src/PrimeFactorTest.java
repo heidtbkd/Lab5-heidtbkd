2362ca4c42ea7de1764f30758125457107275d89
Karl Heidtbrink <heidtbkd@rose-hulman.edu>
2011-03-24 15:14:12 -0400

    Here is the empty version of the Eclipse project with a JUnit test, making sure that JUnit works.

---
+++ Lab5/src/PrimeFactorTest.java
@@ -0,0 +1,13 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+
+public class PrimeFactorTest {
+	@Test
+	public void testJUnit()
+	{
+		assertTrue("Hope this does not Minge.", true);
+	}
+}
