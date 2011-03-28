c9542b690ebbcbf4395dbcda4a6bfe393d50d2f9
Karl Heidtbrink <heidtbkd@rose-hulman.edu>
2011-03-28 16:02:17 -0400

    This contains my edited version of prime to return an ArrayList now and to work with list for 2.

--- Lab5/src/PrimeFactor.java
+++ Lab5/src/PrimeFactor.java
@@ -1,8 +1,18 @@
+import java.util.ArrayList;
+
 
 public class PrimeFactor {
 	
-	public static Object prime (int i)
+	public static ArrayList<Integer> prime (int i)
 	{
+		ArrayList<Integer> temp = new ArrayList<Integer>();
+		
+		if (i > 1)
+		{
+			temp.add(i);
+			return temp;
+		}
+		
 		return null;
 	}
 }
