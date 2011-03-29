ade0111c6e6e251f3bcf89fc5b1a966fcb288a46
Karl Heidtbrink <heidtbkd@rose-hulman.edu>
2011-03-28 21:22:03 -0400

    This contains the solution Matt came up with for prime generator of the first 100 primes, it works

--- Lab5/src/PrimeGeneratorTest.java
+++ Lab5/src/PrimeGeneratorTest.java
@@ -3,67 +3,57 @@ import java.util.ArrayList;
 
 import org.junit.Test;
 
-
-
 public class PrimeGeneratorTest {
 
 	@Test
-	public void testJUnit()
-	{
+	public void testJUnit() {
 		assertTrue("Hope this does not Minge.", true);
 	}
-	
+
 	@Test
-	public void testPrimeGeneratorForOne()
-	{
+	public void testPrimeGeneratorForOne() {
 		assertEquals(PrimeGenerator.generatePrimes(1), new ArrayList<Integer>());
 	}
-	
+
 	@Test
-	public void testPrimeGeneratorForTwo()
-	{
+	public void testPrimeGeneratorForTwo() {
 		assertEquals(PrimeGenerator.generatePrimes(2), new ArrayList<Integer>());
 	}
-	
+
 	@Test
-	public void testPrimeGeneratorForThree()
-	{
+	public void testPrimeGeneratorForThree() {
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		list.add(2);
 		assertEquals(PrimeGenerator.generatePrimes(3), list);
 	}
-	
+
 	@Test
-	public void testPrimeGeneratorForFour()
-	{
+	public void testPrimeGeneratorForFour() {
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		list.add(2);
 		list.add(3);
 		assertEquals(PrimeGenerator.generatePrimes(4), list);
 	}
-	
+
 	@Test
-	public void testPrimeGeneratorForFive()
-	{
+	public void testPrimeGeneratorForFive() {
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		list.add(2);
 		list.add(3);
 		assertEquals(PrimeGenerator.generatePrimes(5), list);
 	}
-	
+
 	@Test
-	public void testPrimeGeneratorForSix()
-	{
+	public void testPrimeGeneratorForSix() {
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		list.add(2);
 		list.add(3);
 		list.add(5);
 		assertEquals(PrimeGenerator.generatePrimes(6), list);
 	}
-	
+
 	@Test
-	public void testPrimeGeneratorForEight()
-	{
+	public void testPrimeGeneratorForEight() {
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		list.add(2);
 		list.add(3);
@@ -71,10 +61,9 @@ public class PrimeGeneratorTest {
 		list.add(7);
 		assertEquals(PrimeGenerator.generatePrimes(8), list);
 	}
-	
+
 	@Test
-	public void testPrimeGeneratorForTwenty()
-	{
+	public void testPrimeGeneratorForTwenty() {
 		ArrayList<Integer> list = new ArrayList<Integer>();
 		list.add(2);
 		list.add(3);
@@ -86,65 +75,19 @@ public class PrimeGeneratorTest {
 		list.add(19);
 		assertEquals(PrimeGenerator.generatePrimes(20), list);
 	}
-	
+
 	@Test
-	public void testPrimeGeneratorForFirstHundred()
-	{
+	public void testPrimeGeneratorForFirstHundred() {
 		ArrayList<Integer> list = new ArrayList<Integer>();
-		int intArray [] = {
-			    2,3,5,7,11,13,17,19,23,29,31,37,41,43,47,53,59,61,67,71,73,79,83,
-			    89,
-			    97,
-			    101,
-			    103,
-			    107,
-			    109,
-			    113,
-			    127,
-			    131,
-			    137,
-			    139,
-			    149,
-			    151,
-			    157,
-			    163,
-			    167,
-			    173,
-			    179,
-			    181,
-			    191,
-			    193,
-			    197,
-			    199,
-			    211,
-			    223,
-			    227,
-			    229,
-			    233,
-			    239,
-			    241,
-			    251,
-			    257,
-			    263,
-			    269,
-			    271,
-			    277,
-			    281,
-			    283,
-			    293,
-			    307,
-			    311,
-			    313,
-			    317,
-			    331,
-			    337,
-			    347,
-			    349,
-			    353,
-			    359,367,373,379,383,389,397,401,409,419,421,431,433,439,443,449,457,461,463,467,479,487,491,499,503,509,521,523,541	
-		};
-		for (int i = 0; i < 99; i++)
-		{
+		int intArray[] = { 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43,
+				47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97, 101, 103, 107, 109,
+				113, 127, 131, 137, 139, 149, 151, 157, 163, 167, 173, 179,
+				181, 191, 193, 197, 199, 211, 223, 227, 229, 233, 239, 241,
+				251, 257, 263, 269, 271, 277, 281, 283, 293, 307, 311, 313,
+				317, 331, 337, 347, 349, 353, 359, 367, 373, 379, 383, 389,
+				397, 401, 409, 419, 421, 431, 433, 439, 443, 449, 457, 461,
+				463, 467, 479, 487, 491, 499, 503, 509, 521, 523, 541 };
+		for (int i = 0; i < 100; i++) {
 			list.add(i, intArray[i]);
 		}
 
