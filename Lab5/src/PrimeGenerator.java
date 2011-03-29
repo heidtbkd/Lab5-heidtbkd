50ca11f304c05216ee0e7f99b48c3f6ef53aad8b
Karl Heidtbrink <heidtbkd@rose-hulman.edu>
2011-03-28 20:25:17 -0400

    This contains the solution for generate primes 3...that I wrote...

--- Lab5/src/PrimeGenerator.java
+++ Lab5/src/PrimeGenerator.java
@@ -4,6 +4,20 @@ import java.util.ArrayList;
 public class PrimeGenerator {
 
 	public static ArrayList<Integer> generatePrimes(int i) {
-		return new ArrayList<Integer>();
+		if(i<=2)
+		{
+			return new ArrayList<Integer>();
+		}
+		else
+		{
+			ArrayList<Integer> temp = new ArrayList<Integer>();
+			
+			if(i==3)
+			{
+				temp.add(2);
+			}
+			
+			return temp;
+		}
 	}
 }
