493bdf4a41bdb9730efc0b13250605f2886950b4
Karl Heidtbrink <heidtbkd@rose-hulman.edu>
2011-03-28 20:05:04 -0400

    Here are the empty class and test class for PrimeGenerator.

---
+++ Lab5/src/PrimeGeneratorTest.java
@@ -0,0 +1,16 @@
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+
+
+public class PrimeGeneratorTest {
+
+	@Test
+	public void testJUnit()
+	{
+		assertTrue("Hope this does not Minge.", true);
+	}
+	
+	
+}
